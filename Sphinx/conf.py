627649743ab9360bb1d06f3ff4a8331f14b54dfb
OPi (Olivier Pirson) <olivier.pirson.opi@gmail.com>
2020-03-27 15:36:25 +0100

    Try to correct problem with Read the Docs.

--- Sphinx/conf.py
+++ Sphinx/conf.py
@@ -34,6 +34,8 @@ autodoc_member_order = 'groupwise'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo',
               'sphinx.ext.coverage', 'sphinx.ext.viewcode']
 
+autodoc_mock_imports = ['audioread', 'matplotlib', 'pygame']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
