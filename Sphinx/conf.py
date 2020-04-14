154cdc46a9023145361e3ea416bd1ab7c3b9a467
OPi (Olivier Pirson) <olivier.pirson.opi@gmail.com>
2020-04-15 01:52:01 +0200

    Try to correct Read the Docs problem.

--- Sphinx/conf.py
+++ Sphinx/conf.py
@@ -42,6 +42,8 @@ autodoc_mock_imports = ['audioread', 'matplotlib', 'pygame',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
